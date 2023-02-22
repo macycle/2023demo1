9a882b5138e369f3d9ffe2cebf35c46ab9b0dba5
fucking <1124942097@qq.com>
2023-02-22 22:14:28 +0800

    新增了css 和 js文件

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#btn").click(function(){
+        $("#wraper").text('xxxxxx').css('border-radius','20px').css('padding','20px')
+    })
+
+    $("#btn").mouseenter(function(){
+        $(this).css('background-color','red')
+    })
+    $("#btn").mouseleave(function(){
+        $(this).css('background-color','green')
+    })
+})
